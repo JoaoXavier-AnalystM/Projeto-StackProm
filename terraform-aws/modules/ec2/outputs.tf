5ca67bfba937d5b0a36056d96297ad9a05c540d8
JOAO XAVIER <joaoxneto7050@gmail.com>
2023-12-07 19:17:52 -0300

    Att vars

--- terraform-aws/modules/ec2/outputs.tf
+++ terraform-aws/modules/ec2/outputs.tf
@@ -1,14 +1,14 @@
 output "instance_state" {
-  description = "The state of the ec2 instance "
+  description = "Status Instância"
   value       = aws_instance.ec2_instance.*.instance_state
 }
 
 output "instance_public_dns" {
-  description = "The Public DNS address of the ec2 instance"
+  description = "Endereço DNS público da instância ec2"
   value       = aws_instance.ec2_instance.*.public_dns
 }
 
 output "instance_public_ip" {
-  description = "The Public Ip address of the ec2 instance"
+  description = "Endereço IP público da instância ec2"
   value       = aws_instance.ec2_instance.*.public_ip
 }
--- terraform-aws/prometheus-stack/outputs.tf
+++ terraform-aws/prometheus-stack/outputs.tf
@@ -1,15 +1,15 @@
 output "instance_state" {
-  description = "The state of the ec2 instance "
+  description = "Status ec2 instance "
   value       = module.ec2.instance_state
 }
 
 output "instance_public_dns" {
-  description = "The Public DNS address of the ec2 instance"
+  description = "Endereço DNS público da instância ec2"
   value       = module.ec2.instance_public_dns
 }
 
 output "instance_public_ip" {
-  description = "The Public Ip address of the ec2 instance"
+  description = "Endereço IP público da instância ec2"
   value       = module.ec2.instance_public_ip
 }
 
